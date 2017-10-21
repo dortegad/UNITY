84f1f699dcb9020d9d18f70441dcb2fbe0e0898b
dortegad <ortega.dav@gmail.com>
2017-10-21 17:06:38 +0200

    avanzando en el ejemplo

--- Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    
+    private Rigidbody rb; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
+
         if (other.tag == "Boundary")
         {
             return;
         }
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            //gameController.GameOver();
+        }
+
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
